be74a3125422ccb122925e2ead68c84bae901708
Brian Elvesæter <brian.elvesater@sintef.no>
2015-10-05 11:25:37 +0200

    Initial commit for ProaSense networking adapter.

---
+++ networking-base/src/main/java/net/modelbased/proasense/adapter/networking/AbstractNetworkingAdapter.java
@@ -0,0 +1,34 @@
+/**
+ * Copyright (C) 2014-2015 SINTEF
+ *
+ *     Brian Elvesæter <brian.elvesater@sintef.no>
+ *     Shahzad Karamat <shazad.karamat@gmail.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package net.modelbased.proasense.adapter.networking;
+
+import net.modelbased.proasense.adapter.base.AbstractBaseAdapter;
+
+import org.apache.log4j.Logger;
+
+public class AbstractNetworkingAdapter extends AbstractBaseAdapter {
+    public final static Logger logger = Logger.getLogger(AbstractFileAdapter.class);
+
+    protected FileConsumerInput inputPort;
+
+
+    protected AbstractFileAdapter() {
+
+    }
+}
